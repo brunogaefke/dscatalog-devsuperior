e8fb0196b7600d89762c7d47ccc18517bfd7e66a
Bruno Gaefke <brunogaefke@gmail.com>
2021-10-04 14:48:48 -0300

    Tests created and passing

---
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,97 @@
+package com.devsuperior.dscatalog.services;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@SpringBootTest
+@Transactional
+public class ProductServiceIT {
+	
+	@Autowired
+	private ProductService service;
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private long countTotalProducts;
+	
+	
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+		
+	}
+	
+	@Test
+	public void FindAllPagedShouldReturnPageWhenPage0Size10() {
+		 
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
+		Assertions.assertTrue(true);
+	}
+	
+	@Test
+	public void FindAllPagedShouldReturnEmptyPageWhenPageDoesNotExists() {
+		 
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void FindAllPagedShouldReturnSortedPageWhenSortByName() {
+		 
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name"));
+		
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+		
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals("Name", result.getSort());
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertTrue(true);
+	
+	}
+	
+	@Test
+	public void deleteShouldDeleteResourceWhenIdExists() {
+		
+		service.delete(existingId);
+		
+		Assertions.assertEquals(countTotalProducts -1, repository.count());
+	}
+	
+	@Test
+	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
+		
+		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+			service.delete(nonExistingId);
+		});
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -0,0 +1,24 @@
+package com.devsuperior.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+public class Factory {
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 800.0, "http://img.com/img.png", Instant.parse("1970-04-12T10:30:00Z"));
+		product.getCategories().add(createCategory());
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(1L, "Electronics");
+	}
+}
