23e9e510945f1f0b273857e884d35559ac7dfffe
Bruno Gaefke <brunogaefke@gmail.com>
2021-11-30 13:40:36 -0300

    Global state with context API

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -2,10 +2,19 @@ import './assets/styles/custom.scss';
 import './App.css';
 
 import Routes from 'Routes';
+import { useState } from 'react';
+import { AuthContex, AuthContextData } from 'AuthContext';
 
 function App() {
+
+  const [authContextData, setAuthContextData] = useState<AuthContextData>({
+    authenticated: false
+  });
+
   return (
+      <AuthContex.Provider value={{authContextData, setAuthContextData}}>
       <Routes />
+      </AuthContex.Provider>
   );
 }
 
