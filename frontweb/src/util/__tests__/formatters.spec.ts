389ef3698cfbf4320e415bf5923fae7c54e2c2c6
brunogaefke <brunogaefke@gmail.com>
2021-12-16 14:47:27 -0300

    Mocking function with spyOn

--- frontweb/src/util/__tests__/formatters.spec.ts
+++ frontweb/src/util/__tests__/formatters.spec.ts
@@ -1,7 +1,22 @@
 import { formatPrice } from '../formatters';
  
-test('formatPrice should format number pt-BR when given 10.1', () => {
-  const result = formatPrice(10.1);
- 
-  expect(result).toEqual('10,10');
-});
+describe('formatPrice tests', () => {
+
+    test('formatPrice should format number pt-BR when given 10.1', () => {
+        const result = formatPrice(10.1);
+       
+        expect(result).toEqual('10,10');
+      });
+
+
+      test('formatPrice should format number pt-BR when given 0', () => {
+        const result = formatPrice(0);
+       
+        expect(result).toEqual('0,00');
+      });
+    });
+
+
+
+
+
