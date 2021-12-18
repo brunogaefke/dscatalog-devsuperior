e7b4e8666f09f2e9917b006560ea07e4adf146e9
brunogaefke <brunogaefke@gmail.com>
2021-12-18 09:03:13 -0300

    MSW mock server, fixtures

--- frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
+++ frontweb/src/pages/Catalog/__tests__/Catalog.spec.tsx
@@ -2,20 +2,23 @@ import { render, screen, waitFor } from "@testing-library/react";
 import { Router } from "react-router-dom";
 import history from 'util/history';
 import Catalog from "..";
+import { server } from './fixtures';
 
+beforeAll(() => { server.listen() })
+afterEach(() => { server.resetHandlers() })
+afterAll(() => { server.close() })
 
 test('should render Catalog with products', async () => {
-  
 
     render(
         <Router history={history}>
-        <Catalog/>
+            <Catalog />
         </Router>
     )
 
     expect(screen.getByText("Catálogo de produtos")).toBeInTheDocument();
 
     await waitFor(() => {
-        expect(screen.getByText("Smart TV")).toBeInTheDocument();
+        expect(screen.getByText("PC Gamer")).toBeInTheDocument();
     })
 });
